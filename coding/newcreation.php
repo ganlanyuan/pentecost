e19edbcfd6535f487f52d3d72bdc84e1f626b97b
designer <ganlanyuan@gmail.com>
2015-05-21 22:00:42 -0400

    add new creation

--- coding/newcreation.php
+++ coding/newcreation.php
@@ -6,37 +6,15 @@
 
 <div class="container">
 	<div class="main">
-	<div class="top">
-		<div id="title">Miraculous Chinese Characters</div>
-		<div id="quote"></div>
-	</div>
-	<div id="toggle">Play</div>
-	<!-- svg start -->
-	<?php include 'assets/img/hanzi.svg'; ?>
-	<!-- svg end -->
+		<!-- svg start -->
+		<?php include 'assets/img/newcreation.svg'; ?>
+		<!-- svg end -->
+		<div class="top">
+			<div id="quote"></div>
+		<!-- <div id="toggle">Play</div> -->
+		</div>
 	</div>
 </div>
-<!-- <div class="container">
-	 <nav class="nav">
-		 <ul>
-			<li><span class="ic-angle-left"></span></li>
-			<li><span class="ic-angle-right"></span></li>
-			<li class="prev"><span class="ic-angle-left"></span></li>
-			<li class="next"><span class="ic-angle-right"></span></li>
-		 </ul>
-	 </nav>
-	<div class="pagination">
-		<span>1</span>
-		<span>2</span>
-		<span>3</span>
-		<span>4</span>
-		<span>5</span>
-		<span>6</span>
-		<span>7</span>
-		<span>8</span>
-	</div>
-</div> -->
-Therefore, if anyone is in Christ, the new creation has come: The old has gone, the new is here! - 2 Corinthians 5:17
 <?php include 'part/footer.php'; ?>
 </body>
 </html>
